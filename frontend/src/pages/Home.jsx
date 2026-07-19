5d05980e259b0bed4432fdb2a283705f4d223341
Ubuntu <ubuntu@ip-172-31-19-55.ec2.internal>
2026-07-19 07:38:27 +0000

    Lesson 2 - Setup project architecture

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold">
+        Project Aurora
+      </h1>
+    </div>
+  )
+}
+
+export default Home
