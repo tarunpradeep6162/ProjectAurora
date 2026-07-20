2c154754d32be54ec06f6cdc4558f19f38d28f34
Ubuntu <ubuntu@ip-172-31-19-55.ec2.internal>
2026-07-20 09:00:37 +0000

    feat: add cinematic typewriter memory section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import HeroSection from "../components/sections/HeroSection";
 import StorySection from "../components/sections/StorySection";
 import MemorySection from "../components/sections/MemorySection";
 import LoveLetterSection from "../components/sections/LoveLetterSection";
+import JourneySection from "../components/sections/JourneySection";
 import BirthdaySection from "../components/sections/BirthdaySection";
 import FooterSection from "../components/sections/FooterSection";
 
@@ -12,6 +13,7 @@ function Home() {
       <StorySection />
       <MemorySection />
       <LoveLetterSection />
+      <JourneySection />
       <BirthdaySection />
       <FooterSection />
     </>
